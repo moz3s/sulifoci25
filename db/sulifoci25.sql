b27ccb9b859d5e8fbd81db0f1fe2910009644883
csutor <csutics07@gmail.com>
2025-03-18 18:33:39 +0100

    database_update

--- db/sulifoci25.sql
+++ db/sulifoci25.sql
@@ -26,8 +26,8 @@ SET default_table_access_method = heap;
 
 CREATE TABLE public.class (
     osztaly character varying(5) NOT NULL,
-    pontszam integer,
-    korosztaly integer
+    korosztaly integer,
+    pontszam integer DEFAULT 0
 );
 
 
@@ -43,7 +43,8 @@ CREATE TABLE public.match (
     o2 character varying(5) NOT NULL,
     date date NOT NULL,
     "time" time without time zone NOT NULL,
-    winner character varying(5)
+    winner character varying(5),
+    bunteto boolean
 );
 
 
@@ -124,19 +125,7 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 -- Data for Name: class; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.class (osztaly, pontszam, korosztaly) FROM stdin;
-09.E	3	1
-11.C	6	2
-10.A	7	1
-11.B	9	2
-09.F	3	1
-12.C	10	2
-10.D	6	1
-11.A	8	2
-09.G	4	1
-12.B	11	2
-10.C	5	1
-11.D	7	2
+COPY public.class (osztaly, korosztaly, pontszam) FROM stdin;
 \.
 
 
@@ -144,16 +133,7 @@ COPY public.class (osztaly, pontszam, korosztaly) FROM stdin;
 -- Data for Name: match; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.match (id, o1, o2, date, "time", winner) FROM stdin;
-5	11.A	11.B	2024-03-10	14:00:00	11.A
-6	10.C	10.D	2024-02-28	15:30:00	10.D
-7	09.E	09.F	2025-03-20	16:00:00	\N
-8	12.A	12.B	2025-03-22	17:00:00	\N
-9	10.E	10.F	2025-03-23	13:30:00	\N
-10	11.C	11.D	2025-03-24	15:30:00	\N
-11	09.E	10.E	2025-03-25	15:00:00	\N
-12	09.E	10.F	2025-03-26	14:30:00	\N
-13	09.B	10.A	2025-03-24	14:30:00	\N
+COPY public.match (id, o1, o2, date, "time", winner, bunteto) FROM stdin;
 \.
 
 
@@ -162,7 +142,7 @@ COPY public.match (id, o1, o2, date, "time", winner) FROM stdin;
 --
 
 COPY public.users (id, name, password) FROM stdin;
-1	admin	$2b$10$LA1B2S0J5LXP0YHQx2ownefUAOiDWtnQ5pm8519MIJ5K5x50xaK42
+1	admin	$2b$10$BEU3KDHELxKhlXzyOJt81eeE8yI8Se9oqB1mlNCo5D5C/3/aJqjZO
 \.
 
 
@@ -170,7 +150,7 @@ COPY public.users (id, name, password) FROM stdin;
 -- Name: match_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.match_id_seq', 13, true);
+SELECT pg_catalog.setval('public.match_id_seq', 1, false);
 
 
 --
